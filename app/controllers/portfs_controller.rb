3a017d55fc167bcdfb1c3653e4439e91435d30a5
Monsmith <Monsmith>
2017-12-22 16:45:08 +0700

    new and create actions for portf

--- app/controllers/portfs_controller.rb
+++ app/controllers/portfs_controller.rb
@@ -2,4 +2,18 @@ class PortfsController < ApplicationController
 def index
   @portfolio_items = Portf.all
 end
+def new
+  @portfolio_item = Portf.new
+end
+def create
+    @portfolio_item = Portf.new(params.require(:portf).permit(:title, :subtitle,:body))
+
+  respond_to do |format|
+    if @portfolio_item.save
+      format.html { redirect_to portfs_path, notice: 'Portfolio was successfully created.' }
+    else
+      format.html { render :new }
+    end
+  end
+end
 end

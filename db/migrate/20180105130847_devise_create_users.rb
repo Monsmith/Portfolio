bcfb1436ed0a5c4602bcc414b7509d5fae6f4007
Monsmith <Monsmith>
2018-01-05 21:59:44 +0700

    Implemented logout

--- db/migrate/20180105130847_devise_create_users.rb
+++ db/migrate/20180105130847_devise_create_users.rb
@@ -7,7 +7,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
-#Custom
+      #Custom
       t.string :name
 
       ## Recoverable
